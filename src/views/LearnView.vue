02fd99494920939ad65efac9191c4904e5a157d2
ajacho <belen.jacho18@gmail.com>
2026-06-06 12:20:02 -0700

    Add lst of commands, need to organize all different OS cmds

--- src/views/LearnView.vue
+++ src/views/LearnView.vue
@@ -1,32 +1,45 @@
 <script setup>
-import { ref } from 'vue'
-import { commands } from '../data/commands.js'
-const selected = ref(commands[0])
+import { ref } from "vue";
+import { commands } from "../data/commands.js";
+const selected = ref(commands[0]);
 const nextCommand = () => {
-    const currentIndex = commands.findIndex(cmd => cmd.command === selected.value.command)
-    const nextIndex = (currentIndex + 1) % commands.length
-    selected.value = commands[nextIndex]
-}
+  const currentIndex = commands.findIndex(
+    (cmd) => cmd.command === selected.value.command,
+  );
+  const nextIndex = (currentIndex + 1) % commands.length;
+  selected.value = commands[nextIndex];
+};
 const prevCommand = () => {
-    const currentIndex = commands.findIndex(cmd => cmd.command === selected.value.command)
-    const prevIndex = (currentIndex - 1 + commands.length) % commands.length
-    selected.value = commands[prevIndex]
-}
+  const currentIndex = commands.findIndex(
+    (cmd) => cmd.command === selected.value.command,
+  );
+  const prevIndex = (currentIndex - 1 + commands.length) % commands.length;
+  selected.value = commands[prevIndex];
+};
 </script>
 
 <template>
-<h1 class= "text-xl pb-4">This is the learn view</h1>
-</br>
-            <div v-if="selected">
-                <h2>{{ selected.command }}</h2>
-                <p>{{ selected.desc }}</p>
-                <p>{{ selected.usage }}</p>
-                <img :src="selected.picture" alt="" width="200" height="200" />
-            </div>
-            <div class="flex justify-between mt-4">        
-                <button class="text-green-500 hover:text-green-300" @click="prevCommand">[Previous]</button>
-                <button class="text-green-500 hover:text-green-300" @click="nextCommand">[Next]</button>
-            </div>
+  <div v-if="selected">
+    <h2 class="text-2xl font-bold mb-4">{{ selected.command }}</h2>
+    <p class="text-gray-300 mb-4">{{ selected.desc }}</p>
+    <p class="text-gray-300">Syntax: {{ selected.usage }}</p>
+    <p class="text-gray-300 mt-4">Real-world Example:</p>
+    <div class="command-box">
+      <code>
+        <span class="text-green-500 rounded text-sm font-mono">
+          &lt;/&gt; Bash </span
+        ><br />
+        <span class="text-green-500">andrea@ubuntu:~$</span>
+        {{ selected.realworldsample }}</code
+      >
+    </div>
+  </div>
+  <div class="flex justify-between mt-4">
+    <button class="text-green-500 hover:text-green-300" @click="prevCommand">
+      [Previous]
+    </button>
+    <button class="text-green-500 hover:text-green-300" @click="nextCommand">
+      [Next]
+    </button>
+  </div>
 </template>
-
-
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,21 +1,33 @@
 <template>
-<h1 class="text-5xl font-bold font-mono text-green-500 mb-5 text-center ">Menu</h1>
+  <h1 class="text-5xl font-bold font-mono text-green-500 mb-5 text-center">
+    Menu
+  </h1>
 
-<div class="card grid grid-cols-1 md:grid-cols-2 gap-4">
-  <div class="bg-gray-800 p-6 rounded-lg shadow-md">
-    <h2 class="text-2xl font-bold mb-4">Learn Commands</h2>
-    <p class="text-gray-300 mb-4">Explore and learn various command-line commands with detailed descriptions and usage examples.</p>
-    <button @click="$router.push('/learn')" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-      Go to Learn
-    </button>
+  <div class="card grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="bg-gray-800 p-6 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold mb-4">Learn Commands</h2>
+      <p class="text-gray-300 mb-4">
+        Explore and learn various command-line commands with detailed
+        descriptions and usage examples.
+      </p>
+      <button
+        @click="$router.push('/learn')"
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Go to Learn
+      </button>
     </div>
-  <div class="bg-gray-800 p-6 rounded-lg shadow-md">
-    <h2 class="text-2xl font-bold mb-4">Game</h2>
-    <p class="text-gray-300 mb-4">Take a break from learning commands and play.</p>
-    <button @click="$router.push('/games')" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-      Go to Games
-    </button>
+    <div class="bg-gray-800 p-6 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold mb-4">Game</h2>
+      <p class="text-gray-300 mb-4">
+        Take a break from learning commands and play.
+      </p>
+      <button
+        @click="$router.push('/games')"
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Go to Games
+      </button>
     </div>
-</div>
-
+  </div>
 </template>
